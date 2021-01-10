776167424fc4f61e7600a168395b464d09552f4e
Alex Dergunov <alex0001.dergunov@yandex.ru>
2021-01-10 23:23:09 +0700

    Alex

--- Modern Web Browser/Program.cs
+++ Modern Web Browser/Program.cs
@@ -14,10 +14,29 @@ namespace Modern_Web_Browser
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new AppContainer());
+            AppContainer container = new AppContainer();
+
+            // Add the initial Tab
+            container.Tabs.Add(
+                // Our First Tab created by default in the Application will have as content the Form1
+                new TitleBarTab(Container)
+                {
+                    Content = new FrmBrowser
+                    {
+                        Text = "New Tab"
+                    }
+                }
+                );
+
+            // Set initial tab the first one
+            container.SelectedTabIndex = 0;
+
+            // Create tabs and start application
+            TitleBarTabsApplicationContext applicationContext = new TitleBarTabsApplicationContext();
+            applicationContext.Start(container);
+            Application.Run(applicationContext);
         }
     }
 }
---
+++ Modern Web Browser/frmBrowser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Modern_Web_Browser
+{
+    public partial class frmBrowser : Form
+    {
+        public frmBrowser()
+        {
+            InitializeComponent();
+        }
+        protected TitleBarTabs ParentTabs
+        {
+            get
+            {
+                return (ParentForm as TitleBarTabs);
+            }
+        }
+    }
+}
